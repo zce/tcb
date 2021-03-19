01d63247e7690fd2e929022080e1f03619eaca1c
zce <w@zce.me>
2021-03-19 19:37:58 +0800

    feat: initial commit

--- functions/qrcode/index.js
+++ functions/qrcode/index.js
@@ -5,8 +5,11 @@ exports.main = async event => {
   const content = text || url
   if (!content) return { statusCode: 400, body: 'Missing required parameter: url.' }
   const qrcode = new QRCode({ content, padding: 0, join: true })
+  const img = Buffer.from(qrcode.svg({ container: 'svg-viewbox' })).toString('base64')
   return {
-    headers: { 'content-type': 'image/svg+xml' },
-    body: qrcode.svg({ container: 'svg-viewbox' })
+    headers: {
+      'content-type': 'text/html'
+    },
+    body: `<img style="display:block;margin:auto;max-width:400px" src="data:image/svg+xml;base64,${img}">`
   }
 }
