171392d41e63c7697f2b75ea9694b81dbc29b1a2
zce <w@zce.me>
2021-07-23 18:16:04 +0800

    feat: cors anywhere

--- functions/cors/index.js
+++ functions/cors/index.js
@@ -1,20 +1,27 @@
 const fetch = require('node-fetch')
 
 exports.main = async ({ path, queryStringParameters }) => {
+  const target = path.substr(1)
+
+  if (!/^https?/.test(target)) return 'cors anywhere'
+
   const search = new URLSearchParams(queryStringParameters)
 
-  const response = await fetch(`${path.substr(1)}?${search}`)
+  const response = await fetch(`${target}?${search}`)
 
-  const { data } = await response.json()
+  const buffer = await response.arrayBuffer()
 
   return {
+    isBase64Encoded: true,
+    statusCode: response.status,
     headers: {
       ...response.headers,
       'Access-Control-Allow-Credentials': true,
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-      'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+      'Content-Type': response.headers.get('Content-Type')
     },
-    body: data
+    body: Buffer.from(buffer).toString('base64')
   }
 }
